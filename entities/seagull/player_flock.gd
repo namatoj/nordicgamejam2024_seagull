ee629e32f71e4f697204e5812bea94106265d8da
Martin Basterrechea <mbs@reshapebiotech.com>
2024-04-20 10:11:37 +0200

    revert position

--- entities/seagull/player_flock.gd
+++ entities/seagull/player_flock.gd
@@ -10,7 +10,7 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		target = event.global_position
+		target = event.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
