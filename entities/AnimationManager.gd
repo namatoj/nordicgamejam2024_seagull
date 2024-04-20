b848b6e35d76240fbaeeea25509df8d29b5dd9c4
Martin Basterrechea <mbs@reshapebiotech.com>
2024-04-20 12:38:02 +0200

    automatic_animations

--- entities/AnimationManager.gd
+++ entities/AnimationManager.gd
@@ -1,27 +1,28 @@
 class_name AnimationManager
 extends Node2D
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var sprite_name: String = "zeppelin"
 
 
-# func _ready():
-# 	var sprite_frames = SpriteFrames.new()
-# 	for n in range(8):
-# 		var dir = str(n)
-# 		var sprite = load("res://art/" + sprite_name + "/" + sprite_name + "_" + dir + ".png")
-# 		sprite_frames.add_animation(dir)
-# 		sprite_frames.add_frame(dir, sprite, 1)
+func _ready():
+	var sprite_frames = SpriteFrames.new()
+	for n in range(16):
+		var dir = str(n)
+		var sprite = load("res://art/" + sprite_name + "/" + sprite_name + "_" + dir + ".png")
+		sprite_frames.add_animation(dir)
+		sprite_frames.add_frame(dir, sprite, 1)
+	animated_sprite.frames = sprite_frames
 
 
 
 func look_towards(radians):
 
-	radians = map_radians_to_circle(radians + PI / 8)
+	radians = map_radians_to_circle(radians + PI / 16)
 
-	var direction : float = remap(radians, 0, 2 * PI, 0, 8)
-	sprite.animation = str(floor(direction))
+	var direction : float = remap(radians, 0, 2 * PI, 0, 16)
+	animated_sprite.animation = str(floor(direction))
 
 
 func map_radians_to_circle(angle):
